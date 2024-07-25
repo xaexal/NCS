324c2bec74ed069029bce6dbb80c8f5d4ee1a9ab
xaexal <xaexal@gmail.com>
2024-07-26 01:51:34 +0900

    202407260151

--- NCS/src/main/java/com/etoile/app/DAO/_Member.java
+++ NCS/src/main/java/com/etoile/app/DAO/_Member.java
@@ -8,4 +8,6 @@ import com.etoile.app.DTO.Member;
 public interface _Member {
 	Member checkUser(String mobile, String password);
 	int saveLoginTime(String mobile);
+	int checkStudent(int mid);
+	int changePasscode(String x, String y, String z);
 }
