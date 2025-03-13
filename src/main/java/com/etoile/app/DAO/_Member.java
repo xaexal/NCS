b55c1d48fbe3b216b8df6eaebc467feebf5d82cd
xaexal <xaexal@gmail.com>
2025-03-13 19:31:32 +0900

    2025-03-13 19:31

--- src/main/java/com/etoile/app/DAO/_Member.java
+++ src/main/java/com/etoile/app/DAO/_Member.java
@@ -8,11 +8,12 @@ import com.etoile.app.DTO.Member;
 @Mapper
 public interface _Member {
 	Member checkUser(@Param("mobile") String mobile, @Param("passcode") String password);
-	int saveLoginTime(@Param("mobile") String mobile);
+	int saveLoginTime(@Param("id") int id);
 	int checkStudent(@Param("member_id") int mid);
 	int changePasscode(@Param("mobile") String x, @Param("passcode") String y, @Param("passcode1") String z);
 	int setTempPasscode(@Param("mobile") String x, @Param("passcode_new") String y);
 	Member get(@Param("mobile") String mobile);
+	Member getPersonal(@Param("id") int id);
 	int insert(@Param("mobile") String mobile, @Param("passcode") String passcode);
 	int updateBySelf(@Param("mobile") String mobile, @Param("name") String name, @Param("passcode") String passcode,
 			@Param("gender") String gender, @Param("birthday") String birthday,
