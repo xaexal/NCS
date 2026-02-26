025825b61607f707c1caa7087a1d890cc4526120
xaexal <xaexal@gmail.com>
2026-02-27 07:58:38 +0900

    20260227 0758

--- src/main/java/com/etoile/app/controller/Common.java
+++ src/main/java/com/etoile/app/controller/Common.java
@@ -15,21 +15,6 @@ public class Common {
 		JSONObject result = new JSONObject();
 		result.put("errcode",Errata.error);
 		result.put("data", null);
-//		try {
-//			HttpSession s = req.getSession();
-//			if (s.getAttribute("memberID") == null) {
-//				result.put("errcode", Errata.signin);
-//				result.put("data", "/signin");
-//				throw new Exception(Message.signIn);
-//			}
-//			req.forEach((k, v) -> System.out.println(k + " [" + v+"]"));
-//			String id=req.getParameter("id");
-//			if(id==null || id.equals("")) throw new Exception("primary key ungiven");
-//			int n = adao.delete(Integer.parseInt(id));
-//			result.put("errcode", Errata.none);
-//		} catch(Exception e) {
-//			result.put("message", e.getMessage());
-//		}
 		return result.toJSONString();
 	}
 }
--- src/main/java/com/etoile/app/controller/CourseController.java
+++ src/main/java/com/etoile/app/controller/CourseController.java
@@ -1,13 +1,10 @@
 package com.etoile.app.controller;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -165,7 +162,7 @@ public class CourseController {
 		return ""+result;
 	}
 	@DeleteMapping("/delete/{cid}")
-	public String doDelete(@PathVariable("cid") int cid) {
+	public String doDelete(@PathVariable int cid) {
 		int result=0;
 		try {
 			ArrayList<Student> arStudent = _std.list(cid,null);
